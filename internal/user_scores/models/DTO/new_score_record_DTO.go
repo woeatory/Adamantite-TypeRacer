d0eea0457b92b2381629ebf63d33b23fed5ce8c2
Yurii Bobal <79090254+woeatory@users.noreply.github.com>
2023-07-18 19:26:44 +0300

    removed required binding for typos count

--- internal/user_scores/models/DTO/new_score_record_DTO.go
+++ internal/user_scores/models/DTO/new_score_record_DTO.go
@@ -3,5 +3,5 @@ package DTO
 type NewScoreRecordDTO struct {
 	WPM      int `json:"WPM" binding:"required"`
 	Accuracy int `json:"Accuracy" binding:"required"`
-	Typos    int `json:"Typos" binding:"required"`
+	Typos    int `json:"Typos"`
 }
